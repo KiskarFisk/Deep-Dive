0916ee6b5cab71532a157eecd9d147020e30feec
KiskarFisk <107329472+KiskarFisk@users.noreply.github.com>
2025-11-26 20:26:14 -0500

    Update weapon_list.py
    Added new weapons, comments, and removed a redundant line of code

--- weapon_list.py
+++ weapon_list.py
@@ -4,8 +4,12 @@ machete = w.Weapon("Machete", 60, "A standard issue HALIDON machete, commonly fo
 axe = w.Weapon("Axe", 90, "A standard axe, used commonly by firemen.")
 pipe = w.Weapon("Lead Pipe", 20, "A lead pipe, nothing more nothing less.")
 knife = w.Weapon("Knife", 40, "A standard knife.")
+sledge = w.Weapon("Sledgehammer", 50, "A large hammer.")
+# Special
 acid = w.Weapon("Acid Vial", 100, "A vial of acid.")
+# Guns
 pistol = w.Weapon("Pistol", 110, "A pistol.")
-bolt_rifle = w.Weapon("Bolt Action Rifle", 150, "An old bolt action rifle, very outdated now.")
-
-t1_weapon = [machete, axe]
+bolt_rifle = w.Weapon("Bolt Action Rifle", 130, "An old bolt action rifle, very outdated now.")
+single_shotgun = w.Weapon("Single Shot Shotgun", 150, "A shotgun with no ammo reserve and a single barrel.")
+double_shotgun = w.Weapon("Double Barrel Shotgun", 160, "A shotgun with two barrels.")
+dart_gun = w.Weapon("Dart Gun", 80, "A gun, that shoots darts.")
