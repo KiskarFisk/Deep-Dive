6c18bc7d297a183d110405fcd1efac1b349f1e70
Kiskar Fisk <leop.2014lp@gmail.com>
2025-11-27 15:56:24 -0500

    First tier 3 missions

---
+++ missions/t3_ml.py
@@ -0,0 +1,7 @@
+import mission_function as miss
+
+infil1 = miss.Mission("Infiltrate Factory", "infiltrate", 3, 3)
+infil2 = miss.Mission("Infiltrate Warehouse", "infiltrate", 3, 2)
+infil3 = miss.Mission("Infiltrate Lab", "infiltrate", 3, 4)
+
+t3_mlist = [infil1, infil2, infil3]
