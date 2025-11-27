c588d932b816a2b28157f1b14f171aec1da01bb8
KiskarFisk <107329472+KiskarFisk@users.noreply.github.com>
2025-11-26 21:21:16 -0500

    Update weapon_list.py
    Added new weapons, a list for shop items, and a function

--- weapon_list.py
+++ weapon_list.py
@@ -1,4 +1,8 @@
 import weapon_function as w
+from player_function import player1
+import random
+
+# TIER I
 
 machete = w.Weapon("Machete", 60, "A standard issue HALIDON machete, commonly found on clones.")
 axe = w.Weapon("Axe", 90, "A standard axe, used commonly by firemen.")
@@ -13,3 +17,18 @@ bolt_rifle = w.Weapon("Bolt Action Rifle", 130, "An old bolt action rifle, very 
 single_shotgun = w.Weapon("Single Shot Shotgun", 150, "A shotgun with no ammo reserve and a single barrel.")
 double_shotgun = w.Weapon("Double Barrel Shotgun", 160, "A shotgun with two barrels.")
 dart_gun = w.Weapon("Dart Gun", 80, "A gun, that shoots darts.")
+# High Tech Bable
+mk0_laz_pistol = w.Weapon("Mk0 Lazer Pistol", 110, "An extremely old lazer pistol with a massive battery, its classified as a rifle because of its size.")
+
+t1_shop_list = [
+    machete,
+    axe,
+
+    pistol,
+
+    mk0_laz_pistol
+]
+
+def generate_shop():
+    if player1.tier == 1:
+        player1.shop_inv.append(random.choice(t1_shop_list))
