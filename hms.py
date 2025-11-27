a0f929a38cd74e58695dd35361aeac4c89f24454
KiskarFisk <107329472+KiskarFisk@users.noreply.github.com>
2025-11-26 21:55:25 -0500

    Update hms.py
    Added a shop

--- hms.py
+++ hms.py
@@ -1,17 +1,20 @@
 import random, time
 from player_function import player1
+import weapon_list as wl
 
 def hms_menu():
     print()
     print("HALIDON Mission Center")
     print("1. Turn in mission")
     print("2. Get a new mission")
+    print("3. Go to the shop")
 
     inp = input("Choice: ")
 
     actions = {
         "1": hms_ti_obj,
-        "2": hms_g_obj
+        "2": hms_g_obj,
+        "3": hms_shop
     }
     action = actions.get(inp)
     if action:
@@ -37,3 +40,36 @@ def hms_g_obj():
         player1.get_objective()
     else:
         print("You already have a mission!")
+
+def hms_shop():
+    print("Shop rotates after every mission.")
+    if player1.last_shop < player1.mis_comp or player1.last_shop == 0:
+        player1.last_shop = player1.mis_comp
+        player1.shop_inv = []
+        for _ in range(3):
+            player1.generate_shop()
+
+    for item in player1.shop_inv:
+        item.display_info()
+        print()
+
+    options = {
+        "1": player1.shop_inv[0],
+        "2": player1.shop_inv[1],
+        "3": player1.shop_inv[2]
+    }
+
+    print("Choose an item by typing its corresponding number, 1 being the top. Type anything else to leave.")
+    print("Warning, buying a weapon will replace your current one permanently.")
+    inp = input("Choice: ")
+    option = options.get(inp)
+    if option:
+        if player1.credits >= option.damage/10:
+            player1.replace_weapon(option)
+            player1.credits -= option.damage/10
+        else:
+            print("Insufficient credits")
+            time.sleep(2)
+    else:
+        print("Leaving")
+        time.sleep(1)
