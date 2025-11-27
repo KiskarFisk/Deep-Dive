d326ba671184360fa89edf0b8ed99b415d02a67d
Kiskar Fisk <leop.2014lp@gmail.com>
2025-11-27 11:08:33 -0500

    Updated the faction dictionary to include the new factions

--- faction_book.py
+++ faction_book.py
@@ -5,6 +5,8 @@ def main():
     print("Here you may read about the groups you may come across during your diving.")
     if player1.tier == 1:
         tier1_factions()
+    if player1.tier == 2:
+        tier2_factions()
 
 def tier1_factions():
     leave = False
@@ -25,6 +27,29 @@ def tier1_factions():
             print("Leaving")
             leave = True
 
+def tier2_factions():
+    leave = False
+    while not leave:
+        print("1. The Rats\n"
+              "2. Tienne\n"
+              "3. Orfast\n"
+              "4. New Order Knights\n"
+              "5. Toreed Corp.\n")
+        inp = input("Choice: ")
+        options = {
+            "1": rat,
+            "2": tienne,
+            "3": orfast,
+            "4": nwo,
+            "5": toreed
+        }
+        option = options.get(inp)
+        if option:
+            option()
+        else:
+            print("Leaving")
+            leave = True
+
 def rat():
     print("The Rats are a group of drug addicted scavengers, they loot and kill with glee.")
     input("Press enter to continue")
@@ -33,4 +58,10 @@ def tienne():
     input("Press enter to continue")
 def orfast():
     print("Orfast is a group operating solely in Terran cities, they're harmless for the most part, when left alone. They fiercely fight for the so-called freedom of humanity on Terra.")
+    input("Press enter to continue")
+def nwo():
+    print("The New Order Knights are a group of fanatic militants. They believe themselves successors of a knightly order, which one they won't tell outsiders. Despite numerous attempts infiltration has proved impossible.")
+    input("Press enter to continue")
+def toreed():
+    print("Toreed Corporation appeared as one of the premier mech and robotics companies. Their initial success was quickly overshadowed by HALIDON.")
     input("Press enter to continue")
