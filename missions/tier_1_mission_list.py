d6a3fa3246ae465b52cbd4f511a993c328a129b6
KiskarFisk <107329472+KiskarFisk@users.noreply.github.com>
2025-11-26 20:19:35 -0500

    Update tier_1_mission_list.py
    Added new missions and removed a redundant line of code

--- missions/tier_1_mission_list.py
+++ missions/tier_1_mission_list.py
@@ -1,8 +1,12 @@
 import mission_function as miss
-import enemies.tier_1_enemy_list as t1e
 
 rat_land1 = miss.Mission("Scavenge in Abandoned Factory", "scavenge", 1, 3)
 rat_land2 = miss.Mission("Scavenge in Abandoned Office", "scavenge", 1, 4)
 rat_land3 = miss.Mission("Scavenge in Abandoned Warehouse", "scavenge", 1, 2)
 
-t1_missions = [rat_land1, rat_land2, rat_land3]
+infil1 = miss.Mission("Infiltrate Old Bunker", "infiltrate", 1, 3)
+infil2 = miss.Mission("Infiltrate Base in the Slums", "infiltrate", 1, 2)
+
+retrieve1 = miss.Mission("Retrieve Stolen Materials", "retrieve", 1, 3)
+
+t1_missions = [rat_land1, rat_land2, rat_land3, infil1, infil2, retrieve1]
