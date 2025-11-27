68b56eb3e5f3594b2d4f39e0db0e6ba852b69ee1
Kiskar Fisk <leop.2014lp@gmail.com>
2025-11-27 09:30:16 -0500

    Added a new mission

--- missions/tier_1_mission_list.py
+++ missions/tier_1_mission_list.py
@@ -8,5 +8,6 @@ infil1 = miss.Mission("Infiltrate Old Bunker", "infiltrate", 1, 3)
 infil2 = miss.Mission("Infiltrate Base in the Slums", "infiltrate", 1, 2)
 
 retrieve1 = miss.Mission("Retrieve Stolen Materials", "retrieve", 1, 3)
+retrieve2 = miss.Mission("Intercept Stolen Materials", "retrieve", 1, 2)
 
-t1_missions = [rat_land1, rat_land2, rat_land3, infil1, infil2, retrieve1]
+t1_missions = [rat_land1, rat_land2, rat_land3, infil1, infil2, retrieve1, retrieve2]
