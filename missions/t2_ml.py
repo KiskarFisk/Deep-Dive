8910b9975b6383f2a6c9f9a5f0d41ce8a90ccaea
Kiskar Fisk <leop.2014lp@gmail.com>
2025-11-27 10:00:20 -0500

    Added t2 missions

---
+++ missions/t2_ml.py
@@ -0,0 +1,7 @@
+import mission_function as miss
+
+infil1 = miss.Mission("Infiltrate Old Factory", "infiltrate", 2, 4)
+infil2 = miss.Mission("Infiltrate Enemy Base", "infiltrate", 2, 3)
+infil3 = miss.Mission("Infiltrate Old Research Lab", "infiltrate", 2, 2)
+
+t2_misslist = [infil1, infil2, infil3]
